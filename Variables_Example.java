c7827d0918e2e8bb539d7d311aef7518759783af
RAJAYADAV22 <165467694+RAJAYADAV22@users.noreply.github.com>
2024-03-31 11:06:30 +0530

    Add files via upload

---
+++ Variables_Example.java
@@ -0,0 +1,15 @@
+class Variables_Example
+{public static void main(String args[])
+{
+byte number1=12;
+byte number2=13;
+if(number1==number2)
+{
+System.out.println(" The given Two numbers are equal");
+}
+else
+{
+System.out.println(" The given Two numbers are not equal");
+}
+}
+}
